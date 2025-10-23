d10a738ca9aaad3933be858a0d5b7b4530a4ba42
Leon Adams <skorpiogitlab@gmail.com>
2025-10-23 07:15:54 -0400

    feat: add the capability to get human selection from rock, paper, scissors

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function getComputerChoice() {
   }
   else return "scissors";
 }
+
+function getHumanChoice() {
+  let selection = prompt("1 for rock\n2 for paper\n3 for scissors");
+  let humanChoice
+  switch(selection) {
+    case "1": 
+      humanChoice = "rock";
+      break;
+    case "2": 
+      humanChoice = "paper"; 
+      break;
+    case "3": 
+      humanChoice = "scissors"; 
+      break;
+  }
+  return (selection, humanChoice);
+}
