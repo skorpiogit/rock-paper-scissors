3165f63f84e664f47256c0597f19cbc353d9aeec
Leon Adams <skorpiogitlab@gmail.com>
2025-10-23 06:56:32 -0400

    feat: include code to chose at random amongst rock, paper, and scissors

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+function getComputerChoice() {
+  let sentinel = Math.random();
+  if ( (0 < sentinel) && (sentinel < 1.0/3.0) ) {
+    return "rock";
+  } 
+  else if ( (1.0/3.0 <= sentinel) && (sentinel < 2.0/3.0)  ) {
+    return "paper";
+  }
+  else return "scissors";
+}
